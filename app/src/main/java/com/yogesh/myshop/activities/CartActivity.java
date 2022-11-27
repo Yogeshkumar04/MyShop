cd3e5d1f6d06df92f32a402bdb74c92055a4c37f
Yogesh Bandhe <yogeshkumarbandhe@gmail.com>
2022-11-27 23:36:23 +0530

    first commit

---
+++ app/src/main/java/com/yogesh/myshop/activities/CartActivity.java
@@ -0,0 +1,78 @@
+package com.yogesh.myshop.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.hishd.tinycart.model.Cart;
+import com.hishd.tinycart.model.Item;
+import com.hishd.tinycart.util.TinyCartHelper;
+import com.yogesh.myshop.adapters.CartAdapter;
+import com.yogesh.myshop.databinding.ActivityCartBinding;
+import com.yogesh.myshop.model.Product;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class CartActivity extends AppCompatActivity {
+
+    ActivityCartBinding binding;
+    CartAdapter adapter;
+    ArrayList<Product> products;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCartBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        products = new ArrayList<>();
+
+        Cart cart = TinyCartHelper.getCart();
+
+        for (Map.Entry<Item, Integer> item : cart.getAllItemsWithQty().entrySet()){
+            Product product = (Product) item.getKey();
+            int quantity = item.getValue();
+            product.setQuantity(quantity);
+
+            products.add(product);
+        }
+
+
+
+        adapter = new CartAdapter(this, products, new CartAdapter.CartListener() {
+            @Override
+            public void onQuantityChanged() {
+                binding.subtotal.setText(String.format("Rs. %.2f", cart.getTotalPrice()));
+            }
+        });
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(this, layoutManager.getOrientation());
+        binding.cartList.setLayoutManager(layoutManager);
+        binding.cartList.addItemDecoration(itemDecoration);
+        binding.cartList.setAdapter(adapter);
+
+        binding.subtotal.setText(String.format("Rs. %.2f", cart.getTotalPrice()));
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CartActivity.this, CheckoutActivity.class));
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+}
---
+++ app/src/main/java/com/yogesh/myshop/activities/PaymentActivity.java
@@ -0,0 +1,34 @@
+package com.yogesh.myshop.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yogesh.myshop.R;
+import com.yogesh.myshop.databinding.ActivityPaymentBinding;
+import com.yogesh.myshop.utils.Constants;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    ActivityPaymentBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPaymentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String orderCode = getIntent().getStringExtra("orderCode");
+
+        binding.webview.setMixedContentAllowed(true);
+        binding.webview.loadUrl(Constants.PAYMENT_URL + orderCode);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+}
